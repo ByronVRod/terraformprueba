24fdb6224bde46a98875e3333d1383550ed16f5d
ByronVRod <123630708+ByronVRod@users.noreply.github.com>
2024-02-07 11:41:06 +0100

    primer cambio
    actualizacion de archivos

--- main.tf
+++ main.tf
@@ -57,3 +57,40 @@ resource "ibm_is_instance" "instance1by" {
   user_data = data.template_cloudinit_config.cloud-init-apptier.rendered
   resource_group = data.ibm_resource_group.rg.id
 }
+
+resource "ibm_is_instance" "instance2by" {
+  name    = "instance2by"
+  image   = var.image
+  profile = var.profile
+  primary_network_interface {
+    subnet = ibm_is_subnet.subnet2.id
+  }
+  vpc  = ibm_is_vpc.vpc1.id
+  zone = var.zone2
+  keys = [data.ibm_is_ssh_key.sshkey1.id]
+  user_data = data.template_cloudinit_config.cloud-init-apptier.rendered
+
+  resource_group = data.ibm_resource_group.rg.id
+}
+
+resource "ibm_is_security_group_rule" "sg1_tcp_rule_22" {
+  group     = ibm_is_vpc.vpc1.default_security_group
+  direction = "inbound"
+  remote    = "0.0.0.0/0"
+  tcp {
+    port_min = "22"
+    port_max = "22"
+  }
+}
+
+resource "ibm_is_security_group_rule" "sg1_tcp_rule_80" {
+  group     = ibm_is_vpc.vpc1.default_security_group
+  direction = "inbound"
+  remote    = "0.0.0.0/0"
+  tcp {
+    port_min = "80"
+    port_max = "80"
+  }
+}
+
+
